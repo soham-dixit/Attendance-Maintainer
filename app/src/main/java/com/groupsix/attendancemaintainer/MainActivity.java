e79e12d339e8eb9ef40ce670f1fe62083a1901ab
sohamdixit <sohamddixit@gmail.com>
2022-04-09 19:28:24 +0530

    initial commit

---
+++ app/src/main/java/com/groupsix/attendancemaintainer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.groupsix.attendancemaintainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
